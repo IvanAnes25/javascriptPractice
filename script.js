8c0ba7d9b09c1ef55ca673d9abb4a09403b4b423
Ivan <ivananes@gmail.com>
2026-09-19 22:49:37 +0800

    first offial prac

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-let word = "Hello World";
+// const timeOut = 120;
 
-console.log(word + "Hi I am Ivan");
+// // let message = "";
+
+// // if (timeOut< 30){
+// //     message = 'You need to try harder';
+// // }
+// // else if (timeOut < 60){
+// //     message = 'U doing Good!';
+// // }
+// // else{
+// //     message = 'Excellent!';
+// // }
+
+// const message = timeOut < 30 ? "You need to try harder" 
+//     :  timeOut < 60 ?'You doin good' 
+//     : 'Excellent!';
+// console.log(message);
+
+const playerGuess = 8;
+const correctAnswer = 6;
+
+/*
+Challenge 
+1. Refactor the if else statement to use a ternary operator.
+*/
+
+// let message = ''
+// if (playerGuess === correctAnswer) {
+//     message = 'Correct!'
+// }
+// else {
+//     message = 'Wrong!'
+// }
+
+// console.log(message)
+
+const message = playerGuess > correctAnswer ? 'too high' : playerGuess < correctAnswer ? 'too low' : 'Correct';
+console.log(message);
