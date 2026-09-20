306ce6662b70aa3a55cab6b74e881f531b1e315f
Ivan <ivananes@gmail.com>
2026-09-20 18:42:56 +0800

    switch case statemements

--- script.js
+++ script.js
@@ -39,3 +39,30 @@
 //     : playerGuess < correctAnswer ? 'too low' 
 //     : 'Correct';
 // console.log(message);
+
+
+//SWITCH STATEMENTS
+
+// function select(item){
+//     let price = 0;
+
+//     switch(item){
+//         case 'coffee':
+//             price = 2;
+//             break;
+//         case 'sandwiches':
+//             price = 5;
+//             break;
+//         case 'salad':
+//             price = 4;
+//             break;
+//         case 'lemon cake':
+//             price = 3;
+//             break;
+//         default:
+//             return `Sorry, we don't sell ${item}`;
+//     }
+//     return `You selected ${item}. That will be $${price}`;
+// }
+// console.log(select('lemon cake'));
+
